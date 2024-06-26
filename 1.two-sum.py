6b75c81d5a9674fc2a910361002c6cd67e1ad916
xinjohnmaomaolen <112685903+gcobc26744@users.noreply.github.com>
2024-06-26 09:43:26 +0800

    python

---
+++ 1.two-sum.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        n = len(nums)
+        for i in range(n - 1):
+            for j in range (i + 1, n):
+                if nums [i] + nums [j] == target:
+                    return [i , j]
+        return [] 
+# @lc code=end
+
