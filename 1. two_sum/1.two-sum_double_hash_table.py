ca64b137c91e53f468aa8df3caeaee97d0c502ba
Olive <gcobc26744@gmail.com>
2024-07-01 23:01:11 +0800

    Two-pass Hash Table

---
+++ 1. two_sum/1.two-sum_double_hash_table.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numMap = {}
+        n = len(nums)
+
+        #Build the hash table
+        for i in range(n):
+            numMap[nums[i]] = i
+
+        # Find the complement
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in numMap and numMap[complement] != i:
+                return [i, numMap[complement]]
+        return [] 
+# @lc code=end
+
