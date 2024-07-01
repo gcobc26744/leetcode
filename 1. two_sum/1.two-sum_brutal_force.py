26c159de4df69016d166e17a7455d9ca54651061
Olive <gcobc26744@gmail.com>
2024-07-01 22:21:38 +0800

    brutal_force

---
+++ 1. two_sum/1.two-sum_brutal_force.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if nums [i] + nums[j] == target:
+                    return [i, j]
+    
+        return []                
+# @lc code=end
+
+    
